734a574f1f5f9433ebecdbd12b29ca71fccc15b5
nataliia-v <nataly.verbenskaya@gmail.com>
2025-05-05 23:13:18 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,12 @@
 
 const table = document.querySelector('table');
 
-for (const row of table.rows) {
+for (let row of table.rows) {
   const cells = row.cells;
-  const positionCell = cells[1].cloneNode(true);
 
-  row.insertBefore(positionCell, cells[4]);
+  if (cells.length >= 5) {
+    const positionCell = cells[1].cloneNode(true);
+    row.insertBefore(positionCell, cells[4]);
+  }
 }
+
